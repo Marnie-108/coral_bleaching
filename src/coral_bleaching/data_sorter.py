4ba9803c0003cdeb20d9c20a379de32098e44c8f
Marnie Paul <marnie.007.paul@outlook.com>
2024-04-12 14:14:14 +0100

    Removed unnecessary commented code.

--- src/coral_bleaching/data_sorter.py
+++ src/coral_bleaching/data_sorter.py
@@ -12,14 +12,6 @@ def main() -> None:
 
     data = umpy.read.from_csv("./coral_bleaching.csv")
 
-    # Check values
-    # count = 0
-    # for i in range(1, len(data)):
-    #     data[i][0] = umpy.convert.str_to_int(data[i][0])
-    #     if not isinstance(data[i][0], int):
-    #         count += 1
-    # print(f"\n count = {count}")
-
     reefs = sorted(data[1:], key=lambda x: int(x[0]))
     umpy.write.to_csv("./coral_bleaching-sorted.csv", reefs, data[0])
 
